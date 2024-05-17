7da24042e1def4738a85dc2e7610a965ab554f2a
mapson <deismapangou@gmail.com>
2024-05-17 16:59:08 +0100

    init

--- src/data/pdf/BaseModel.js
+++ src/data/pdf/BaseModel.js
@@ -11,99 +11,118 @@ export const attestationPdf = async (e) => {
   const docDefinition = {
     pageSize: "A4",
     pageOrientation: "landscape",
-    pageMargins: [90, 150, 20, 50],
+    pageMargins: [10, 150, 20, 50],
     content: [
-    //   {
-    //     text: e["infos"],
-    //     relativePosition: { x: 460, y: 55 },
-    //   },
-    //   {
-    //     text: e["agence"],
-    //     absolutePosition: { x: 380, y: 230 },
-    //   },
-    //   {
-    //     text: e["adress"],
-    //     absolutePosition: { x: 500, y: 230 },
-    //   },
-    //   {
-    //     text: e["police"],
-    //     absolutePosition: { x: 290, y: 250 },
-    //   },
-    //   {
-    //     stack: [
-    //       {
-    //         text: e["rc"],
-    //         absolutePosition: { x: 290, y: 280 },
-    //       },
-    //       {
-    //         text: e["incendie"],
-    //         absolutePosition: { x: 290, y: 310 },
-    //       },
-    //       {
-    //         text: e["dateDebut"],
-    //         absolutePosition: { x: 270, y: 330 },
-    //       },
-    //     ],
-    //   },
-    //   {
-    //     stack: [
-    //       {
-    //         text: e["dr"],
-    //         absolutePosition: { x: 370, y: 280 },
-    //       },
-    //       {
-    //         text: e["dom"],
-    //         absolutePosition: { x: 370, y: 310 },
-    //       },
-    //       {
-    //         text: e["dateFin"],
-    //         absolutePosition: { x: 385, y: 330 },
-    //       },
-    //     ],
-    //   },
-    //   {
-    //     stack: [
-    //       {
-    //         text: e["vol"],
-    //         absolutePosition: { x: 450, y: 280 },
-    //       },
-    //       {
-    //         text: e["bg"],
-    //         absolutePosition: { x: 450, y: 310 },
-    //       },
-    //     ],
-    //   },
+      {
+        text: e["infos"],
+        relativePosition: { x: 430, y: 55 },
+      },
+      {
+        text: e["agence"],
+        absolutePosition: { x: 250, y: 230 }, // Déplacer vers la gauche en ajustant x à 250
+      },
+      {
+        text: e["adress"],
+        absolutePosition: { x: 420, y: 230 },
+      },
+      {
+        text: e["id"],
+        absolutePosition: { x: 700, y: 230 },
+        fontSize: 10,
+      },
+      {
+        text: e["police"],
+        absolutePosition: { x: 190, y: 250 }, // Déplacer vers la gauche en ajustant x à 190
+        fontSize: 10,
+      },
+      {
+        text: e["dateDebut"],
+        absolutePosition: { x: 690, y: 250 }, // Déplacer vers la gauche en ajustant x à 510
+        fontSize: 10,
+      },
+      {
+        text: e["dateFin"],
+        absolutePosition: { x: 750, y: 250 },
+        fontSize: 10,
+      },
+      {
+        text: e["immat"],
+        absolutePosition: { x: 700, y: 280 },
+        fontSize: 10,
+      },
+      {
+        stack: [
+          {
+            text: e["rc"],
+            absolutePosition: { x: 140, y: 280 },
+            fontSize: 10,
+          },
+          {
+            text: e["incendie"],
+            absolutePosition: { x: 140, y: 310 },
+            fontSize: 10,
+          },
+          {
+            text: e["dateDebut"],
+            absolutePosition: { x: 120, y: 330 },
+            fontSize: 10,
+          },
+        ],
+      },
+      {
+        stack: [
+          {
+            text: e["dr"],
+            absolutePosition: { x: 220, y: 280 },
+            fontSize: 10,
+          },
+          {
+            text: e["dom"],
+            absolutePosition: { x: 220, y: 310 },
+            fontSize: 10,
+          },
+          {
+            text: e["dateFin"],
+            absolutePosition: { x: 225, y: 330 },
+            fontSize: 10,
+          },
+        ],
+      },
+      {
+        stack: [
+          {
+            text: e["vol"],
+            absolutePosition: { x: 320, y: 280 },
+            fontSize: 10
+          },
+          {
+            text: e["bg"],
+            absolutePosition: { x: 320, y: 310 }, // Déplacer vers la gauche en ajustant x à 350
+            fontSize: 10
+          },
+        ],
+      },
       {
         text: e["genre"],
-        absolutePosition: { x: 500, y: 330 },
+        absolutePosition: { x: 390, y: 330 }, // Déplacer vers la gauche en ajustant x à 430
+        fontSize: 10
       },
-    //   {
-    //     text: e["type"],
-    //     absolutePosition: { x: 550, y: 350 },
-    //   },
-    //   {
-    //     text: e["marque"],
-    //     absolutePosition: { x: 670, y: 350 },
-    //   },
-    //   {
-    //     text: e["immat"],
-    //     absolutePosition: { x: 730, y: 350 },
-    //   },
+      {
+        text: e["type"],
+        absolutePosition: { x: 410, y: 330 }, // Déplacer vers la gauche en ajustant x à 450
+        fontSize: 10
+      },
+      {
+        text: e["marque"],
+        absolutePosition: { x: 510, y: 330 }, // Déplacer vers la gauche en ajustant x à 540
+        fontSize: 10
+      },
+      {
+        text: e["immat"],
+        absolutePosition: { x: 610, y: 330 }, // Déplacer vers la gauche en ajustant x à 650
+         fontSize: 10
+      },
     ],
-    styles: {
-      anotherStyle: {
-        alignment: "left",
-        margin: [0, 0, 0, 5],
-      },
-      anotherStyle2: {
-        alignment: "center",
-        margin: [0, 50, 0, 5],
-      },
-      anotherStyle3: {
-        alignment: "right",
-        margin: [0, -30, 0, 5],
-      },
-    },
   };
 
   pdfMake.createPdf(docDefinition).open();
